1831a7608a063d3f9b3dce7e42f591c50b099150
tumluliu <nudtlliu@gmail.com>
2016-12-08 15:08:33 +0100

    remove the router name validation because that has been done in the __init__.py

--- rap/servicefactory.py
+++ rap/servicefactory.py
@@ -10,19 +10,11 @@ from .graphhopper import GraphHopperRouter
 # from . import tomtom
 """ Factory method of creating concrete routing service instances
 """
-VALID_ROUTING_SERVICES = [
-    'mapbox', 'graphhopper', 'mapzen', 'google', 'here', 'tomtom'
-]
 
 
 def RoutingServiceFactory(service_name):
-    if service_name not in VALID_ROUTING_SERVICES:
-        # TODO(lliu): Here should throw an exception or deal with it in an
-        # identical way
-        return None
-    with open('../apikeys.json', 'r') as f:
+    with open('routerconf.json', 'r') as f:
         service_provider_conf = json.load(f)
-
     # TODO(lliu): The following stuff of course should be prettified
     # ATTENTION!! A pile of ugly things are coming...
     if service_name == 'mapbox':
