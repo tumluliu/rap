37a4910de2a68ad9f35329d9bc3acbbddb97b72e
tumluliu <nudtlliu@gmail.com>
2016-12-12 17:04:31 +0100

    format adjustments, finish the functions

--- rap/mb.py
+++ rap/mb.py
@@ -5,9 +5,7 @@ from uritemplate import URITemplate
 from .base import RoutingService
 
 
-class MapboxRouter(
-        RoutingService
-):
+class MapboxRouter(RoutingService):
     """ Wrapper class of Mapbox direction http API v5
 
     The mapbox-sdk-py is not used because it's out of date
@@ -21,48 +19,27 @@ class MapboxRouter(
     """
 
     v5_baseuri = "https://api.mapbox.com/directions/v5"
-    api_uri_template = URITemplate(
-        v5_baseuri
-        +
-        "{/profile}{/coordinates}"
-    )
+    api_uri_template = URITemplate(v5_baseuri + "{/profile}{/coordinates}")
     profile_dict = {
-        "driving":
-        "mapbox/driving",
-        "walking":
-        "mapbox/walking",
-        "cycling":
-        "mapbox/cycling"
+        "driving": "mapbox/driving",
+        "walking": "mapbox/walking",
+        "cycling": "mapbox/cycling"
     }
 
-    def __init__(
-            self,
-            api_key,
-            profile,
-            cache=None
-    ):
+    def __init__(self, api_key, profile, cache=None):
         self.token = api_key
-        if profile not in self.profile_dict.keys(
-        ):
+        if profile not in self.profile_dict.keys():
             # The profile passed in is not supported by Mapbox routing service
             return None
-        self.profile = self.profile_dict[
-            profile]
-        super(
-        ).__init__(
-            self,
-            api_key,
-            cache
-        )
+        self.profile = self.profile_dict[profile]
+        super().__init__(self, api_key, cache)
 
-    def find_path(
-            self,
-            source_lng,
-            source_lat,
-            target_lng,
-            target_lat,
-            params=None
-    ):
+    def find_path(self,
+                  source_lng,
+                  source_lat,
+                  target_lng,
+                  target_lat,
+                  params=None):
         """ Find the optimal path with Mapbox Directions HTTP API
 
         :param source_lng: longitude value of the starting position
@@ -71,32 +48,19 @@ class MapboxRouter(
         :param target_lat: latitude value of the ending position
         :param params: the other query parameters for the mapbox router
         """
-        self.coordinates = "{0},{1};{2},{3}".format(
-            source_lng,
-            source_lat,
-            target_lng,
-            target_lat
-        )
+        self.coordinates = "{0},{1};{2},{3}".format(source_lng, source_lat,
+                                                    target_lng, target_lat)
         self.params = params
+        self.params.update({'access_token': self.token})
 
-        # TODO(lliu): fetch the routes via mapbox direction api calling
         uri = self.api_uri_template.expand({
-            'profile':
-            self.
-            profile,
-            'coordinates':
-            self.
-            coordinates
+            'profile': self.profile,
+            'coordinates': self.coordinates
         })
         # query_str = URIVariable('?' + ','.join(self.params.keys()))
         # query_str.expand(self.params)
         # uri += query_str
-        resp = self.session.get(
-            uri,
-            params=params
-        )
-        self.handle_http_error(
-            resp
-        )
+        resp = self.session.get(uri, params=params)
+        self.handle_http_error(resp)
         resp.geojson = resp.json
         return resp
