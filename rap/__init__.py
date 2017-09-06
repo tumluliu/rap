fe750e2049e9f3532d39a8ad5d9bcbae6042cba5
Lu Liu <nudtlliu@gmail.com>
2017-09-06 04:44:54 +0200

    leave mapbox, ors and google as available providers

--- rap/__init__.py
+++ rap/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'LIU Lu'
 __contact__ = 'nudtlliu@gmail.com'
 __license__ = 'MIT'
 __all__ = [
-    'base', 'servicefactory', 'mapbox', 'ors', 'graphhopper', 'google',
+    'base', 'servicefactory', 'mapbox', 'ors', 'google',
 ]
 
 from .servicefactory import RoutingServiceFactory
--- rap/servicefactory.py
+++ rap/servicefactory.py
@@ -4,27 +4,28 @@
 import json
 import logging
 from .mapbox import MapboxRouter
-from .graphhopper import GraphHopperRouter
+from .ors import OpenRouteServiceRouter
+from .google import GoogleMapsRouter
 
-# from . import mapzen
-# from . import google
-# from . import here
-# from . import tomtom
-
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 def RoutingServiceFactory(service_name, profile):
-    logger.debug("Create concrete router for {0} with profile {1}".format(
-        service_name, profile))
+    """ Factary method for creating concrete router instance
+    """
+    LOGGER.debug("Create concrete router for %s with profile %s",
+                 service_name, profile)
     with open('routerconf.json', 'r') as f:
         service_provider_conf = json.load(f)
     # TODO(lliu): The following stuff of course should be prettified
     # ATTENTION!! A pile of ugly things are coming...
     if service_name == 'mapbox':
-        logger.info("Create mapbox router")
+        LOGGER.info("Create mapbox router")
         return MapboxRouter(profile, service_provider_conf['mapbox']['key'])
-    elif service_name == 'graphhopper':
-        logger.info("Create graphhopper router")
-        return GraphHopperRouter(profile,
-                                 service_provider_conf['graphhopper']['key'])
+    elif service_name == 'openrouteservice':
+        LOGGER.info("Create openrouteservice router")
+        return OpenRouteServiceRouter(profile,
+                                      service_provider_conf['openrouteservice']['key'])
+    elif service_name == 'google':
+        LOGGER.info("Create google maps router")
+        return GoogleMapsRouter(profile, service_provider_conf['google']['key'])
