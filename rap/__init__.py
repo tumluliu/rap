f950c547cf3b5f8994ce09c3a446647ed4786941
Lu Liu <nudtlliu@gmail.com>
2017-09-06 04:40:21 +0200

    remove the inexisted classes

--- rap/__init__.py
+++ rap/__init__.py
@@ -1,11 +1,10 @@
 __title__ = 'rap'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'LIU Lu'
 __contact__ = 'nudtlliu@gmail.com'
 __license__ = 'MIT'
 __all__ = [
-    'base', 'servicefactory', 'mapbox', 'graphhopper', 'mapzen', 'google',
-    'here', 'tomtom'
+    'base', 'servicefactory', 'mapbox', 'ors', 'graphhopper', 'google',
 ]
 
 from .servicefactory import RoutingServiceFactory
